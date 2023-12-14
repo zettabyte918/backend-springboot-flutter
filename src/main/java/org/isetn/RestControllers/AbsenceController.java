c101cc17309cce01bb8cddb45700a1e895ca1144
hossem <hammamihossem99@gmail.com>
2023-12-14 12:47:16 +0100

    up

--- src/main/java/org/isetn/RestControllers/AbsenceController.java
+++ src/main/java/org/isetn/RestControllers/AbsenceController.java
@@ -1,5 +1,8 @@
 package org.isetn.RestControllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import org.isetn.entities.Absence;
@@ -11,6 +14,7 @@ import org.isetn.repository.MatiereRepository;
 import org.isetn.repository.AbsenceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +31,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AbsenceController {
 	@Autowired
 	private AbsenceRepository AbsenceRepository;
+
+	@GetMapping("/getByMatiereIdAndDate")
+	public List<Absence> findByMatiereIdAndDate(
+			@RequestParam Long matiereId,
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+		LocalDateTime startDateTime = LocalDateTime.of(date, LocalTime.MIN);
+		LocalDateTime endDateTime = LocalDateTime.of(date, LocalTime.MAX);
+
+		return AbsenceRepository.findByMatiereMatiereIdAndDateBetween(matiereId, startDateTime, endDateTime);
+	}
 
 	@GetMapping("/getByEtudiantId/{etudiantId}")
 	public List<Absence> findByClasseIdClas(@PathVariable("etudiantId") Long etudiantId) {
--- src/main/java/org/isetn/repository/AbsenceRepository.java
+++ src/main/java/org/isetn/repository/AbsenceRepository.java
@@ -1,15 +1,22 @@
 package org.isetn.repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.isetn.entities.Absence;
 import org.isetn.entities.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AbsenceRepository extends JpaRepository<Absence, Long> {
     // You can add custom query methods here if needed
     List<Absence> findByEtudiantId(Long codClass);
+
+    List<Absence> findByMatiereMatiereIdAndDateBetween(Long matiereId, LocalDateTime startDate, LocalDateTime endDate);
 }
